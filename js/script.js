7cfa93a308d9b33b530dc3c1b3703d37d44a06eb
James Hoofard <james.hoofard.iii@gmail.com>
2021-08-10 14:23:59 -0500

    oh my god so many bugs so little space

--- js/script.js
+++ js/script.js
@@ -1,38 +1,61 @@
-var questionOne = [answerOneOne, answerTwoOne, answerThreeOne, answerFourOne]
-var questionTwo = [answerOneTwo, answerTwoTwo, answerThreeTwo, answerFourTwo]
-var questionThree = [answerOneThree, answerTwoThree, answerThreeThree, answerFourThree]
-var answerOneOne = document.getElementById("answer1-1")
-var answerTwoOne = document.getElementById("answer2-1")
-var answerThreeOne = document.getElementById("answer3-1")
-var answerFourOne = document.getElementById("answer4-1")
-var answerOneTwo = document.getElementById("answer1-2")
-var answerTwoTwo = document.getElementById("question2-2")
-var answerThreeTwo = document.getElementById("question3-2")
-var answerFourTwo = document.getElementById("question4-2")
-var answerOneThree = document.getElementById("question1-3")
-var answerTwoThree = document.getElementById("question2-3")
-var answerThreeThree = document.getElementById("question3-3")
-var answerFourThree = document.getElementById("answer4-3")
-var startButton = document.getElementsByClassName("startbutton")
-var resetButton = document.getElementsByClassName("resetbutton")
-var saveButton = document.getElementsByClassName("savebutton")
+
+var answerOne = document.querySelector("#answer1")
+var answerTwo = document.querySelector("#answer2")
+var answerThree = document.querySelector("#answer3")
+var startButton = document.querySelector(".startbutton")
+var resetButton = document.querySelector(".resetbutton")
+var saveButton = document.querySelector(".savebutton")
 var timeLeft = 60
 var possibleCorrectAnswers = [0, 1, 2, 3]
 var actualCorrectAnswers = []
+var result = document.getElementsByClassName("result");
 
 //up next, defining the correct answers with if, if else, else statements.  
 
-startButton.addEventListener ("click", startQuiz)
+var submitButtonOne = document.querySelector("#submit1")
+submitButtonOne.addEventListener("click", console.log("click"))
 
-function startQuiz (){
-     function questionOneFun(){
-         if (addEventListener("click", answerFourOne)){
-         (correctAnswers++);
-         console.log(correctAnswers);
-         console.log(questionOneFun);}
-
-         else (addEventListener("click", answerOneOne, answerOneTwo, answerThreeOne));{
-             console.log("Wrong Answer")
-         }
-
-        }}
+function questionOneFun(){
+         var resultStatement;
+       if (answerOne === 4){
+        resultStatement = "Incorrect";
+        console.log(resultStatement);
+       } else if (answerOne !== 4){
+            resultStatement = "Correct";
+            console.log(resultStatement)
+       } 
+         result.innerText = resultStatement;
+        }
+        function questionTwoFun(){
+            var resultStatement;
+          if (answerTwoTwo.checked || answerFourTwo.che2cked || answerOneTwo.checked){
+           resultStatement = "Incorrect";
+           console.log(resultStatement)
+          }
+          else if (answerThreeTwo){
+               resultStatement = "Correct";
+               console.log(resultStatement)
+              }
+          
+            result.innerText = resultStatement;
+           }
+           function questionThreeFun(){
+            var resultStatement;
+          if (answerFourThree.checked || answerThreeThree.checked || answerTwoThree.checked){
+           resultStatement = "Incorrect";
+           console.log(resultStatement)
+          }
+           else if (answerOneThree){
+               resultStatement = "Correct";
+               console.log(resultStatement)
+              }
+          
+            result.innerText = resultStatement;
+           }
+            
+            
+                     
+        function connected() {
+            console.log("connected")
+        }
+ connected()
