e168876972e81cd37f380bad19001a2e1898e921
James Hoofard <james.hoofard.iii@gmail.com>
2021-08-10 15:54:23 -0500

    updated my logical statements so they all work, got started on a clock feature

--- js/script.js
+++ js/script.js
@@ -6,56 +6,65 @@ var startButton = document.querySelector(".startbutton")
 var resetButton = document.querySelector(".resetbutton")
 var saveButton = document.querySelector(".savebutton")
 var timeLeft = 60
+var timer = document.querySelector("#timer")
 var possibleCorrectAnswers = [0, 1, 2, 3]
+var highScores = []
 var actualCorrectAnswers = []
 var result = document.getElementsByClassName("result");
 
 //up next, defining the correct answers with if, if else, else statements.  
 
 var submitButtonOne = document.querySelector("#submit1")
-submitButtonOne.addEventListener("click", console.log("click"))
-
+submitButtonOne.addEventListener("click", questionOneFun)
+console.log(answerOne)
 function questionOneFun(){
          var resultStatement;
-       if (answerOne === 4){
-        resultStatement = "Incorrect";
+       if (answerOne.value === "4"){
+        resultStatement = "Correct";
         console.log(resultStatement);
-       } else if (answerOne !== 4){
-            resultStatement = "Correct";
+       } else {
+            resultStatement = "Incorrect";
             console.log(resultStatement)
        } 
          result.innerText = resultStatement;
         }
+        var submitButtonTwo = document.querySelector("#submit2")
+submitButtonTwo.addEventListener("click", questionTwoFun)
         function questionTwoFun(){
             var resultStatement;
-          if (answerTwoTwo.checked || answerFourTwo.che2cked || answerOneTwo.checked){
-           resultStatement = "Incorrect";
-           console.log(resultStatement)
-          }
-          else if (answerThreeTwo){
-               resultStatement = "Correct";
-               console.log(resultStatement)
-              }
+            if (answerTwo.value === "15"){
+                resultStatement = "Correct";
+                console.log(resultStatement);
+               } else {
+                    resultStatement = "Incorrect";
+                    console.log(resultStatement)
+               } 
+                 result.innerText = resultStatement;
+                
           
             result.innerText = resultStatement;
            }
+           var submitButtonThree = document.querySelector("#submit3")
+submitButtonThree.addEventListener("click", questionThreeFun)
            function questionThreeFun(){
             var resultStatement;
-          if (answerFourThree.checked || answerThreeThree.checked || answerTwoThree.checked){
-           resultStatement = "Incorrect";
-           console.log(resultStatement)
-          }
-           else if (answerOneThree){
-               resultStatement = "Correct";
-               console.log(resultStatement)
-              }
-          
-            result.innerText = resultStatement;
+            if (answerThree.value === "5"){
+                resultStatement = "Correct";
+                console.log(resultStatement);
+               } else {
+                    resultStatement = "Incorrect";
+                    console.log(resultStatement)
+               } 
+                 result.innerText = resultStatement;
+                
            }
             
-            
-                     
-        function connected() {
-            console.log("connected")
-        }
- connected()
+         
+startButton.addEventListener("click", timer)
+function timer() {
+    console.log()
+    timer.value = "timeLeft" + timeLeft
+    
+}
+//setIntervalTimer
+//create a var for score, increase by one for correct answers, save score in local storage their name and score. 
