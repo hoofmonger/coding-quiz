22e3ca41144351ab6520e1e4b85849958581de7f
James Hoofard <james.hoofard.iii@gmail.com>
2021-08-10 18:18:16 -0500

    it kinda works, geniunly don't understand how to make the rest work, will revisit

--- js/script.js
+++ js/script.js
@@ -3,16 +3,23 @@ var answerOne = document.querySelector("#answer1")
 var answerTwo = document.querySelector("#answer2")
 var answerThree = document.querySelector("#answer3")
 var startButton = document.querySelector(".startbutton")
+var starButtonId= document.querySelector("#startbutton")
 var resetButton = document.querySelector(".resetbutton")
 var saveButton = document.querySelector(".savebutton")
-var timeLeft = 60
-var timer = document.querySelector("#timer")
+var timerId = 60
+var timerEl = document.querySelector("#timer")
 var possibleCorrectAnswers = [0, 1, 2, 3]
 var highScores = []
 var actualCorrectAnswers = []
-var result = document.getElementsByClassName("result");
+var result = document.querySelector("#result");
 
 //up next, defining the correct answers with if, if else, else statements.  
+
+
+    
+
+
+
 
 var submitButtonOne = document.querySelector("#submit1")
 submitButtonOne.addEventListener("click", questionOneFun)
@@ -20,11 +27,11 @@ console.log(answerOne)
 function questionOneFun(){
          var resultStatement;
        if (answerOne.value === "4"){
-        resultStatement = "Correct";
-        console.log(resultStatement);
+        resultStatement = "Correct!";
+        window.alert("Correct");
        } else {
             resultStatement = "Incorrect";
-            console.log(resultStatement)
+            window.alert("Incorrect");
        } 
          result.innerText = resultStatement;
         }
@@ -33,11 +40,11 @@ submitButtonTwo.addEventListener("click", questionTwoFun)
         function questionTwoFun(){
             var resultStatement;
             if (answerTwo.value === "15"){
-                resultStatement = "Correct";
-                console.log(resultStatement);
+                resultStatement = "Correct!";
+                window.alert("Correct")
                } else {
                     resultStatement = "Incorrect";
-                    console.log(resultStatement)
+                    window.alert("Incorrect")
                } 
                  result.innerText = resultStatement;
                 
@@ -49,22 +56,42 @@ submitButtonThree.addEventListener("click", questionThreeFun)
            function questionThreeFun(){
             var resultStatement;
             if (answerThree.value === "5"){
-                resultStatement = "Correct";
+                resultStatement = "Correct!";
                 console.log(resultStatement);
+                window.alert("Correct")
                } else {
                     resultStatement = "Incorrect";
                     console.log(resultStatement)
+                    window.alert("Incorrect")
                } 
                  result.innerText = resultStatement;
                 
            }
             
          
-startButton.addEventListener("click", timer)
-function timer() {
-    console.log()
-    timer.value = "timeLeft" + timeLeft
-    
+startButton.addEventListener("click", countdown)
+function countdown() {
+    var timeLeft = 60;
+    var timeInterval = setInterval(function () {
+        if (timeLeft > 1) {
+        timerEl.textContent = timeLeft + ' seconds remaining'; 
+        timeLeft--;
+        } else {
+            timerEl.textContent = '';
+            clearInterval(timeInterval);
+            displayMessage();
+     }
+    }, 1000)
+   }   console.log(countdown)
+
+resetButton.addEventListener("click", reset)
+
+function reset(){
+location.reload()
 }
+
+
+  
+  
 //setIntervalTimer
 //create a var for score, increase by one for correct answers, save score in local storage their name and score. 
